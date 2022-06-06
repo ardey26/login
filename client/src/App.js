0d12544e74af7b5ee336bbd971dc341a15cc2f75
Andeus <71312401+ardey26@users.noreply.github.com>
2022-06-06 16:48:20 +0800

    ui overhaul of login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,71 @@
 import React, { useState } from 'react';
-const axios = require('axios').default;
+
+import { Login } from './components/login';
+import { Register } from './components/register';
 
 function App() {
 
-  // USER CREDENTIALS
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState({username: "", password: ""});
+  const loginPicture = require("./assets/images/login.jpg")
 
-  // STATE CHANGES
-  const handleUserChange = (e) => {
-    setUsername(e.target.value);
-    console.log("username: " + username);
-  }
+  const [blur, setBlur] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    console.log("password: " + password);
-  }
+  const toggleBlur = () => {
+    const toggledBlur = !blur;
+    setBlur(toggledBlur);
 
-  const handleLogIn = async (e) => {
+    if(blur){
+      document.body.style.opacity = 0.5;
+    }
+
+    else{
+      document.body.style.opacity = 1;
+    }
     
-    // POST REQUEST RETURNS A STATUS CODE
-    axios.post('http://localhost:5000/login/', {username: username, password: password})
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((res) => {
-      console.log("User doesn't exist")
-    })
-    e.preventDefault();
   }
-
-  const handleRegister = async (e) => {
+  
+  const handleLogin = () =>{
     
-    axios.post('http://localhost:5000/register/', {username: username, password: password})
-    .then((res)=>{
-      console.log(res)
-    })
-    e.preventDefault();
+    toggleBlur();
+    setShowRegister(false); 
+    setShowLogin(!showLogin);
+
   }
 
-
+  const handleRegister = () =>{
+    
+    toggleBlur();
+    setShowLogin(false);
+    setShowRegister(!showRegister);
+    
+  }
   return (
     <div className="App">
-      <form method="POST" action="http://localhost:5000/login" onSubmit={handleLogIn}> 
-        <label> Username </label>
-        <input type="text" onChange={handleUserChange} autoComplete="off" required name="username" placeholder="Enter username here..." />
-
-        <br />
-
-        <label> Password </label>
-        <input type="password" onChange={handlePasswordChange} required name="password" placeholder="Enter password here..." />
-
-        <br />
-        <input type="submit"/>
-      </form>
-
-      <button onClick={handleRegister}> Register </button>
-      <br>
-      </br>
+      { showLogin ? <Login /> : null}
+      { showRegister ? <Register /> : null}
+      <div className="row" style={{margin: '5%', borderStyle: 'solid', borderColor: 'rgb(52,58,64)'}}>
+        <div className="col-lg-8 col-xl-9 col-xxl-8 offset-0 offset-xxl-0" style={{background: 'var(--bs-gray-dark)', padding: '15%', margin: '0px'}}>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 style={{textAlign: 'center', fontWeight: 'bold', color: '#ffffff'}}>KardBan</h1>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row" style={{paddingTop: '5%'}}>
+              <div className="col-md-12 col-xxl-4 offset-xxl-2" style={{paddingLeft: '10px'}}><button className="btn btn-outline-light btn-lg" type="button" style={{width: '100%'}} onClick={handleLogin}>Sign in</button></div>
+              <div className="col-xxl-4 offset-xxl-0"><button className="btn btn-outline-light btn-lg" type="button" style={{width: '100%', marginRight: '20%'}} onClick={handleRegister}>Sign up</button></div>
+            </div>
+          </div>
+          <div />
+          <p />
+        </div>
+        <div className="col" style={{paddingRight: 0, paddingLeft: 0, background: '#343a40'}}>
+          <picture><img className="img-thumbnail" src={loginPicture} style={{height: '100%', padding: 0}} alt="Login"/></picture>
+        </div>
+      </div>
     </div>
   );
 }
